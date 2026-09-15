c9bfc088dbf32cf841ce5a0ac3c8cc664c3c7744
Calvin Tack <calvintack09@gmail.com>
2026-09-15 14:23:09 -0400

    Lab 4 update

--- lab3.py
+++ lab3.py
@@ -1,4 +1,6 @@
 name = "Calvin Tack"
-numbers = {1, 2, 3, 4, 5}
+numbers = {1000, 2000, 3000, 4000, 5000}
+campus = "Boca Raton"
 print("Student:", name)
 print("Numbers:", numbers)
+print("Campus:", campus)
